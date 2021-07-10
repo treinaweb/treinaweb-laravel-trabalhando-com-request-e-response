fdddf3be0dfff29be009131426bc01523ebe9a74
Elton Fonseca <elton869@gmail.com>
2021-07-10 09:09:23 -0300

    02 - Criação do projeto

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ClienteController extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        dd('cheguei aqui');
+    }
+}
